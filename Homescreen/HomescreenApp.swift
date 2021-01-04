7127e37e4b5496d08879bfa58fb6798b5666e56a
Joep Hinderink <hello@joephinderink.nl>
2021-01-04 16:30:31 +0100

    Initial Commit

---
+++ Homescreen/HomescreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomescreenApp.swift
+//  Homescreen
+//
+//  Created by Joep Hinderink on 04/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct HomescreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
