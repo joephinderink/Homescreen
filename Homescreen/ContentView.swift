53b6474af699a207227ade66cdee73ab5a78337a
Joep Hinderink <hello@joephinderink.nl>
2021-01-04 16:43:31 +0100

    Update

--- Homescreen/ContentView.swift
+++ Homescreen/ContentView.swift
@@ -1,6 +1,6 @@
 //
 //  ContentView.swift
-//  Homescreen
+//  Expandable button
 //
 //  Created by Joep Hinderink on 04/01/2021.
 //
@@ -8,9 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isExpanded = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            ZStack {
+                List(1...10, id: \.self) { i in
+                    Text("Row \(i)")
+                }
+                VStack {
+                    Spacer()
+                    HStack {
+                        Spacer()
+                        if isExpanded {
+                            NavigationLink(
+                                destination: Test(),
+                                label: {
+                                    Image(systemName: "photo")
+                                })
+                            .padding()
+                        }
+                        Button(action: {
+                            withAnimation {
+                                isExpanded.toggle()
+                            }
+                        }, label: {
+                            Image(systemName: "ellipsis")
+                                .frame(width: 50, height: 65)
+                        })
+                        Spacer()
+                        if isExpanded {
+                            NavigationLink(
+                                destination: Test(),
+                                label: {
+                                    Image(systemName: "camera")
+                                })
+                            .padding()
+                        }
+                    }
+                    .background(Color.white)
+                    .cornerRadius(50)
+                    .shadow(radius: 10)
+                }
+                .frame(width: 65)
+            }
+            .navigationTitle("Demo")
+            .listStyle(PlainListStyle())
+        }
     }
 }
 
